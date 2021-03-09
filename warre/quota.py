01e27a2332449bee710772c650071923354d73b9
Sam Morrison <sorrison@gmail.com>
2021-03-09 12:59:05 +1100

    Initial code, flavor and reservation base
    Change-Id: I6d16aa3eae2e8504f83afdbd9c77332540d53967

---
+++ warre/quota.py
@@ -0,0 +1,44 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_limit import limit
+
+from warre import models
+from warre.extensions import db
+
+
+_ENFORCER = None
+
+
+def get_enforcer():
+    global _ENFORCER
+    if not _ENFORCER:
+        _ENFORCER = limit.Enforcer(get_usage)
+    return _ENFORCER
+
+
+def get_usage(project_id, resource_names):
+    return {x: get_usage_by_project(project_id, x) for x in resource_names}
+
+
+def get_usage_by_project(project_id, resource):
+    if resource == 'reservation':
+        return db.session.query(models.Reservation) \
+            .filter_by(project_id=project_id).count()
+    if resource == 'hours':
+        total = 0
+        reservations = db.session.query(models.Reservation) \
+            .filter_by(project_id=project_id).all()
+        for r in reservations:
+            total += r.total_hours
+        return total
---
+++ warre/tests/unit/api/v1/test_reservation.py
@@ -0,0 +1,81 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import datetime
+
+from warre.extensions import db
+from warre import models
+from warre.tests.unit import base
+
+
+class TestReservationAPI(base.ApiTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.flavor = self.create_flavor()
+
+    def test_list_reservations(self):
+        self.create_reservation(flavor_id=self.flavor.id,
+                                start=datetime.datetime(2021, 1, 1),
+                                end=datetime.datetime(2021, 1, 2))
+        response = self.client.get('/api/v1/reservations/')
+
+        self.assert200(response)
+        results = response.get_json().get('results')
+        self.assertEqual(1, len(results))
+
+    def test_list_reservations_non_project(self):
+        self.create_reservation(flavor_id=self.flavor.id,
+                                start=datetime.datetime(2021, 1, 1),
+                                end=datetime.datetime(2021, 1, 2),
+                                project_id="notmine")
+        response = self.client.get('/api/v1/reservations/')
+
+        self.assert200(response)
+        results = response.get_json().get('results')
+        self.assertEqual(0, len(results))
+
+
+class TestAdminReservationAPI(TestReservationAPI):
+
+    ROLES = ['admin']
+
+    def test_list_reservations_all_projects(self):
+        self.create_reservation(flavor_id=self.flavor.id,
+                                start=datetime.datetime(2021, 1, 1),
+                                end=datetime.datetime(2021, 1, 2),
+                                project_id='123')
+        self.create_reservation(flavor_id=self.flavor.id,
+                                start=datetime.datetime(2021, 1, 1),
+                                end=datetime.datetime(2021, 1, 2),
+                                project_id='987')                        
+        response = self.client.get('/api/v1/reservations/?all_projects=1')
+
+        self.assert200(response)
+        results = response.get_json().get('results')
+        self.assertEqual(2, len(results))
+
+    def test_list_reservations_other_project(self):
+        self.create_reservation(flavor_id=self.flavor.id,
+                                start=datetime.datetime(2021, 1, 1),
+                                end=datetime.datetime(2021, 1, 2),
+                                project_id='123')
+        self.create_reservation(flavor_id=self.flavor.id,
+                                start=datetime.datetime(2021, 1, 1),
+                                end=datetime.datetime(2021, 1, 2),
+                                project_id='987')                        
+        response = self.client.get('/api/v1/reservations/?all_projects=1')
+
+        self.assert200(response)
+        results = response.get_json().get('results')
+        self.assertEqual(2, len(results))
