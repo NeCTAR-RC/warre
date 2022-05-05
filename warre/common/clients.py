862d3acb66b989d8e68db5a3bfd65d5bcf1f5679
Sam Morrison <sorrison@gmail.com>
2022-05-05 15:36:39 +1000

    Remove unused methods in clients module
    Change-Id: I955308ba229a657001dbeacbb934bccfb46bff7c

--- warre/common/clients.py
+++ warre/common/clients.py
@@ -11,37 +11,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from blazarclient import client as blazarclient
-from keystoneauth1.identity import v3
-from keystoneauth1 import session
 from keystoneclient.v3 import client as ks_client
-from openstack import connection
-from oslo_config import cfg
-from oslo_log import log as logging
-
-
-LOG = logging.getLogger(__name__)
-CONF = cfg.CONF
-
-NOVA_VERSION = '2.60'
-
-
-def get_session(token, project_id):
-    auth = v3.Token(token=token,
-                    auth_url=CONF.keystone.auth_url,
-                    project_id=project_id,
-                    project_domain_id='default')
-    return session.Session(auth=auth)
 
 
 def get_admin_keystoneclient(sesh):
     return ks_client.Client(session=sesh)
-
-
-def get_openstack_client(project_id, token):
-    auth_session = get_session(token, project_id)
-    return connection.Connection(session=auth_session)
-
-
-def get_blazar_client(sesh):
-    return blazarclient.Client(session=sesh, service_type='reservation')
